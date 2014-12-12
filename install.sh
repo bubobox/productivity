ff66b9efd1dd18e059fb667b6ba68dfbb00175d5
Wim <wim@sitebase.be>
2014-12-12 16:55:58 +0100

    add installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+
+# Make sure to start clean
+rm -rf /tmp/productivity-master.zip
+rm -rf /tmp/productivity-master
+
+# Download the source
+curl -o /tmp/productivity-master.zip -s https://codeload.github.com/bubobox/productivity/zip/master
+
+# Extract
+@unzip /tmp/productivity-master.zip -d /tmp
+
+# Build and install the productivity apps
+cd /tmp/productivity-master
+make
+
+echo "Installation done!"
